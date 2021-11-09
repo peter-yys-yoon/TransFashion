df9b1221437a432f4595f0334cefd74ca71d1659
peteryoon <name>
2021-11-09 21:09:42 +0900

    deleted, useless files

--- run.sh
+++
@@ -1,4 +0,0 @@
-clear
-#python tools/train.py --cfg ./experiments/coco/transpose_h/TP_H_w48_256x192_stage3_1_4_d96_h192_relu_enc6_mh1.yaml
-# python tools/train.py --cfg ./experiments/coco/transpose_r/TP_R_256x192_d256_h1024_enc3_mh8_org.yaml
-python tools/train.py --cfg ./experiments/coco/transpose_r/TP_R_256x192_d256_h1024_enc3_mh8_scale.yaml
--- tmp.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-
-a = np.linspace(.5, 0.95, np.round((0.95 - .5) / .05) + 1, endpoint=True)
